686131e16c3570b10d407473dac58d5a513ec530
David <davit.vyan@mail.ru>
2022-03-11 20:18:48 +0400

    Server creating

---
+++ Server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// server and database connction
+const PORT = process.env.PORT;
+const DB_CONNCTION = process.env.DB_CONNECTION;
+
+async function startServer() {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server listening on Port --> ${PORT}`);
+    });
+    await mongoose.connect(DB_CONNCTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+startServer();

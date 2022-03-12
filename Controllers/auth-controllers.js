f7be05138e422da9c72ba011d8e4286e6fe774a7
DavidVardzelian <david.vardzelian@gmail.com>
2022-03-12 23:23:14 +0400

    Check to exists user from username and email

--- Controllers/auth-controllers.js
+++ Controllers/auth-controllers.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 async function createUser(req, res) {
   try {
     const { username, password, email } = req.body;
-    const userExists = await userModel.findOne({ username });
+    const userExists = await userModel.findOne({$or:[{username}, {email}]});
     //Check to user Exists in Database
     if (userExists) {
       res.send("alredy exists");

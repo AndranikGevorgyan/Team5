99fe19e0a00593b64b455acb3affd363c92eae4c
DavidVardzelian <david.vardzelian@gmail.com>
2022-03-12 22:15:34 +0400

    Refactoring

---
+++ Controllers/auth-controllers.js
@@ -0,0 +1,27 @@
+const userModel = require("../Models/UserModel");
+const bcrypt = require("bcrypt");
+
+async function createUser(req, res) {
+  try {
+    const { username, password, email } = req.body;
+    const userExists = await userModel.findOne({ username });
+    //Check to user Exists in Database
+    if (userExists) {
+      res.send("alredy exists");
+      return;
+    }
+    //Password Hashing
+    const hashedPassword = await bcrypt.hash(password, 7);
+    const user = new userModel({
+      username: username,
+      password: hashedPassword,
+      email: email,
+    });
+    user.save();
+    res.send("user success created");
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = { createUser };
